98594b57c111b313d0591d0fa0d6b46712e137bf
root <kolya_tanev@mail.ru>
2025-05-08 14:38:47 +0300

    fix link auth

--- src/pages/login/login-api.js
+++ src/pages/login/login-api.js
@@ -2,7 +2,7 @@ import {domain} from '@api/api'
 
 
 const Login = async (login, password) => {
-    return fetch(`${domain}/user/auth`, {
+    return fetch(`${domain}/users/auth`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
